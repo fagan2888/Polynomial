46bbe5bc9e1551276ac5dbf1d844d8564c177d1a
abhirup-mondal <abhirup.git@gmail.com>
2015-07-31 22:08:39 +0530

    Error Correction

--- term.h
+++ term.h
@@ -7,7 +7,7 @@ private:
 	unsigned int 	_pow;
 
 public:
-	Term() : {};
+	Term() : _coef(0), _pow(0) {};
 	Term(long double a_n, unsigned int n) : _coef(a_n), _pow(n) {};
 	Term(const Term& copy) : _coef(copy._coef), _pow(copy._pow) {};
 };
